d0072e9846c929a1f4f1a77ae4dc221ee687a996
amy <amy>
2023-04-01 09:51:14 +0800

    实现readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,17 +1,29 @@
-import { track, trigger } from "./effect";
+import { track, trigger } from './effect'
 
 export function reactive(row) {
   return new Proxy(row, {
     get: function (target, key, receiver) {
       //   console.log(target, key, receiver)
       let res = Reflect.get(target, key)
-      track(target, key);
+      track(target, key)
       return res
     },
     set(target, key, value, receiver) {
       const res = Reflect.set(target, key, value)
-      trigger(target, key);
+      trigger(target, key)
       return res
+    },
+  })
+}
+
+export function readonly(row) {
+  return new Proxy(row, {
+    get: function (target, key, receiver) {
+      let res = Reflect.get(target, key)
+      return res
+    },
+    set(target, key, value, receiver) {
+      return true
     },
   })
 }
