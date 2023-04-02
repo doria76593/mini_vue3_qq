3f3895966acc9b954443c8d1398314a0c931f1e0
amy <amy>
2023-04-02 23:30:13 +0800

    ref 测试3

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,11 +1,14 @@
-import { hasChanged } from '../shared'
+import { hasChanged, isObject } from '../shared'
 import { isTracking, trackEffects, triggerEffects } from './effect'
+import { reactive } from './reactive'
 
 class RefImpl {
   private _value: any
   public dep
+  private _rawValue: any
   constructor(value) {
-    this._value = value
+    this._rawValue = value
+    this._value = convert(value)
     this.dep = new Set()
   }
   get value() {
@@ -16,11 +19,15 @@ class RefImpl {
   }
   set value(newValue) {
     // 一定是先去修改value的值 再去促发依赖
-    if (hasChanged(newValue, this._value)) {
-      this._value = newValue
+    if (hasChanged(newValue, this._rawValue)) {
+      this._rawValue = newValue
+      this._value = convert(newValue)
       triggerEffects(this.dep)
     }
   }
+}
+function convert(value) {
+  return isObject(value) ? reactive(value) : value
 }
 
 export function ref(value) {
