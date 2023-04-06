797327977126a81390ccebdf9fedc77ee858a589
qingqing <qingqing@qq.com>
2023-04-06 15:58:59 +0800

    重新提交

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -18,28 +18,22 @@ describe('effect', () => {
     user.age++
     expect(nextAge).toBe(12)
   })
+
   it('should return runner when call effect', () => {
-    // 调用effect后 会返回一个function(被称为runner).
-    // 当调用runner的的时候，会再次执行传递给effect的fn;fn调用的时候，会把fn的返回值return出去
-
-    // 总结： 当调用 runner 的时候可以重新执行 effect.run
-    //        runner 的返回值就是用户给的 fn 的返回值
-
-    let foo = 10
+    // 当调用 runner 的时候可以重新执行 effect.run
+    // runner 的返回值就是用户给的 fn 的返回值
+    let foo = 0
     const runner = effect(() => {
       foo++
-      return 'foo'
+      return foo
     })
 
-    expect(foo).toBe(11)
-    const r = runner()
-    expect(foo).toBe(12)
-    expect(r).toBe('foo')
+    expect(foo).toBe(1)
+    runner()
+    expect(foo).toBe(2)
+    expect(runner()).toBe(3)
   })
-<<<<<<< HEAD
 
-=======
->>>>>>> 1fbdd87b879b7b7fe47d48fedad1771c466ba16d
   it('scheduler', () => {
     let dummy
     let run: any
@@ -58,15 +52,6 @@ describe('effect', () => {
     // should be called on first trigger
     obj.foo++
     expect(scheduler).toHaveBeenCalledTimes(1)
-<<<<<<< HEAD
-    // should not run yet
-    expect(dummy).toBe(1)
-    // manually run
-    run()
-    // should have run
-    expect(dummy).toBe(2)
-  })
-=======
     // // should not run yet
     expect(dummy).toBe(1)
     // // manually run
@@ -84,7 +69,7 @@ describe('effect', () => {
     obj.prop = 2
     expect(dummy).toBe(2)
     stop(runner)
-    // obj.prop = 3
+    // obj.prop = 3;
     obj.prop++
     expect(dummy).toBe(2)
 
@@ -111,5 +96,4 @@ describe('effect', () => {
     stop(runner)
     expect(onStop).toBeCalledTimes(1)
   })
->>>>>>> 1fbdd87b879b7b7fe47d48fedad1771c466ba16d
 })
