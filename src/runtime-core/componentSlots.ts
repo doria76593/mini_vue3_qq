ed04409510dcbba891a0089462552244ec40b503
qingqing <qingqing@qq.com>
2023-04-13 11:25:26 +0800

    优化代码

--- src/runtime-core/componentSlots.ts
+++ src/runtime-core/componentSlots.ts
@@ -1,12 +1,25 @@
 export function initSlots(instance, children) {
-  console.log(instance, children)
-  // instance.slots = Array.isArray(children) ? children : [children]
-  const slots = {}
+  // console.log(instance, children)
+
+  // const slots = {}
+  // for (const key in children) {
+  //   const value = children[key]
+  //   slots[key] = normalizeSlotValue(value)
+  // }
+  // console.log('slots22')
+  // console.log(slots)
+  // instance.slots = slots
+
+  normalizeObjectSlots(children, instance.slots)
+}
+
+function normalizeObjectSlots(children: any, slots: any) {
   for (const key in children) {
     const value = children[key]
-    slots[key] = Array.isArray(value) ? value : [value]
+    slots[key] = normalizeSlotValue(value)
   }
-  console.log('slots22')
-  console.log(slots)
-  instance.slots = slots
+}
+
+function normalizeSlotValue(value) {
+  return Array.isArray(value) ? value : [value]
 }
